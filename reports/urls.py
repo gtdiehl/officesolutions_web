9d8b7381c279d9302673f6c8f3684ec4c6b5892e
Greg <34967045+gtdiehl@users.noreply.github.com>
2019-11-27 17:03:22 -0800

    Updated files

--- reports/urls.py
+++ reports/urls.py
@@ -4,4 +4,5 @@ from . import views
 urlpatterns = [
     path("", views.index, name="index"),
     path("<int:report_id>", views.report, name="report"),
+    path("reports", views.reports, name="reports"),
 ]
--- reports/views.py
+++ reports/views.py
@@ -9,11 +9,13 @@ import pandas as pd
 
 
 def index(request):
+    return render(request, "reports/index.html")
+
+def reports(request):
     context = {
         "reports": Reports.objects.all()
     }
-    return render(request, "reports/index.html", context)
-
+    return render(request, "reports/report.html", context)
 
 def report(request, report_id):
     try:
@@ -21,53 +23,111 @@ def report(request, report_id):
     except Reports.DoesNotExist:
         raise Http404("Report does not exist.")
 
-    categories, values_y1, values_y2, table_content, title, template = graph(report_id)
+    input_from = request.GET.get('from').split("/")
+    input_to = request.GET.get('to').split("/")
 
-    context = {"categories": categories, 'values_y1': values_y1, 'values_y2': values_y2, 'table_data': table_content,
-               'report': report, 'title': title}
+    categories, values_y1, values_y2, values_y3, table_content, title, template, title_axis_y1, title_axis_y2, title_axis_y3, title_axis_x,\
+        title_series_names = graph(report_id, int(input_from[0]), int(input_from[1]), int(input_to[0]), int(input_to[1]))
+
+    context = {"categories": categories, 'values_y1': values_y1, 'values_y2': values_y2, 'values_y3': values_y3, 'table_data': table_content,
+               'report': report, 'title': title, 'from_month': input_from[0], 'from_year': input_from[1],
+               'to_month': input_to[0], 'to_year': input_to[1], 'title_axis_y1': title_axis_y1,
+               'title_axis_y2': title_axis_y2, 'title_axis_y3': title_axis_y3, 'title_axis_x': title_axis_x, 'title_series_names': title_series_names}
     return render(request, template, context=context)
 
 
-def graph(report_num):
+def graph(report_num, from_month, from_year, to_month, to_year):
 
     xl = pd.ExcelFile(os.path.join(os.path.dirname(__file__), "data/SalesDataFull.xlsx"))
     df = xl.parse("Orders")
 
     if report_num == 1:
-        rs = profit_of_ten_products_ave(df, 1, 2015, 1, 2015, False)
+        rs = profit_of_ten_products_ave(df, from_month, from_year, to_month, to_year, False)
         title = "Top 10 Products"
         categories = list(rs["Product Name"])
+        title_axis_y1 = "Average Profit Per Unit ($ USD)"
+        title_axis_y2 = ""
+        title_axis_y3 = ""
+        title_axis_x = ""
+        title_series_names = "Product Name"
         values_y1 = list(rs["Average Profit/Unit"])
         values_y2 = ""
-        template = "reports/report.html"
+        values_y3 = ""
+        template = "reports/report_single_column.html"
     elif report_num == 2:
-        rs = profit_of_ten_products_ave(df, 1, 2015, 1, 2015, True)
+        rs = profit_of_ten_products_ave(df, from_month, from_year, to_month, to_year, True)
         title = "Bottom 10 Products"
         categories = list(rs["Product Name"])
+        title_axis_y1 = "Average Profit Per Unit ($ USD)"
+        title_axis_y2 = ""
+        title_axis_y3 = ""
+        title_axis_x = ""
+        title_series_names = "Product Name"
         values_y1 = list(rs["Average Profit/Unit"])
         values_y2 = ""
-        template = "reports/report.html"
+        values_y3 = ""
+        template = "reports/report_single_column.html"
     elif report_num == 3:
-        rs = active_customer_report(df, 1, 2015, 1, 2015, False)
+        rs = active_customer_report(df, from_month, from_year, to_month, to_year, False)
         title = "Most Profitable Customer Report"
         categories = list(rs["Customer Name"])
+        title_axis_y1 = "Profit ($ USD)"
+        title_axis_y2 = "Total Number of Orders"
+        title_axis_y3 = ""
+        title_axis_x = "Customer Name"
+        title_series_names = "Profit,Total Number of Orders"
         values_y1 = list(rs["Profit"])
         values_y2 = list(rs["Total Num of Orders"])
-        template = "reports/report_two_y_axis_2.html"
+        values_y3 = ""
+        template = "reports/report_two_y_axis.html"
     elif report_num == 4:
-        rs = active_customer_report(df, 1, 2015, 1, 2015, True)
+        rs = active_customer_report(df, from_month, from_year, to_month, to_year, True)
         title = "Least Profitable Customer Report"
         categories = list(rs["Customer Name"])
+        title_axis_y1 = "Profit ($ USD)"
+        title_axis_y2 = "Total Number of Orders"
+        title_axis_y3 = ""
+        title_axis_x = "Customer Name"
+        title_series_names = "Profit,Total Number of Orders"
         values_y1 = list(rs["Profit"])
         values_y2 = list(rs["Total Num of Orders"])
-        template = "reports/report_two_y_axis_2.html"
+        values_y3 = ""
+        template = "reports/report_two_y_axis.html"
+    elif report_num == 5:
+        rs = sales_and_profits_by_region(df, from_month, from_year, to_month, to_year)
+        title = "Sales and Profits by Region"
+        categories = list(rs["Region"])
+        title_axis_y1 = "Dollar Amount ($ USD)"
+        title_axis_y2 = ""
+        title_axis_y3 = ""
+        title_axis_x = "Region"
+        title_series_names = "Profit,Sales"
+        values_y1 = list(rs["Profit"])
+        values_y2 = list(rs["Sales"])
+        values_y3 = ""
+        template = "reports/report_grouped_columns.html"
+    elif report_num == 6:
+        discount_values, region_values, series_data, category_values, charts_drilldown_list, sub_category_values, \
+            charts_drilldown_sub_list = discounts_by_region(df, from_month, from_year, to_month, to_year)
+        title = "Discounts Given out by Region"
+        categories = region_values
+        title_axis_y1 = "Percentage of Orders"
+        title_axis_y2 = category_values
+        title_axis_y3 = sub_category_values
+        title_axis_x = "Region"
+        title_series_names = discount_values
+        values_y1 = series_data
+        values_y2 = charts_drilldown_list
+        values_y3 = charts_drilldown_sub_list
+        template = "reports/report_stacked_columns.html"
 
     table_content = df.to_html(index=None)
     table_content = table_content.replace("", "")
     table_content = table_content.replace('class="dataframe"', "class='table table-striped'")
     table_content = table_content.replace('border="1"', "")
 
-    return categories, values_y1, values_y2, table_content, title, template
+    return categories, values_y1, values_y2, values_y3, table_content, title, template, title_axis_y1, title_axis_y2, title_axis_y3, title_axis_x,\
+           title_series_names
 
 
 def profit_of_ten_products_ave(df, from_month, from_year, to_month, to_year, sort):
@@ -102,6 +162,69 @@ def active_customer_report(df, from_month, from_year, to_month, to_year, sort):
     filtered_customers_sum['Total Num of Orders'] = filtered_customers_sum['Customer Name'].map(num_of_orders_dic)
     filtered_customers_sum = filtered_customers_sum.sort_values(by="Profit", ascending=sort)
     return filtered_customers_sum[:10]
+
+
+def sales_and_profits_by_region(df, from_month, from_year, to_month, to_year):
+    df = _filter_df_by_date(df, "Order Date", from_month, from_year, to_month,
+                            to_year)
+
+    df = df[['Sales', 'Profit', 'Region']]
+    df_groupby_sales = df.groupby(['Region']).sum()
+    return df_groupby_sales.reset_index()
+
+
+def discounts_by_region(df, from_month, from_year, to_month, to_year):
+    df = _filter_df_by_date(df, "Order Date", from_month, from_year, to_month,
+                            to_year)
+
+    df['Discount'] = pd.Series(["{0:.2f}%".format(val * 100) for val in df['Discount']], index=df.index)
+
+    discount_values = sorted(df['Discount'].unique(), reverse=True)
+    region_values = sorted(df['Region'].unique(), reverse=True)
+    category_values = sorted(df['Category'].unique(), reverse=True)
+    sub_category_values = sorted(df['Sub-Category'].unique(), reverse=True)
+
+    series_data = []
+    for discount in discount_values:
+        l = []
+        for region in region_values:
+            a = df[(df['Region'] == f'{region}')]
+            a = a[(a['Discount'] == f'{discount}')]
+            l.append(len(a.index))
+        series_data.append(l)
+
+    charts_drilldown_list = []
+
+    for region in region_values:
+        series_drilldown_data = []
+        z = df[(df['Region'] == f"{region}")]
+        for discount in discount_values:
+            l = []
+            for category in category_values:
+                a = z[(z['Category'] == f'{category}')]
+                a = a[(a['Discount'] == f'{discount}')]
+                l.append(len(a.index))
+            series_drilldown_data.append(l)
+        charts_drilldown_list.append(series_drilldown_data)
+
+    charts_drilldown_sub_list = []
+
+    for region in region_values:
+        series_drilldown_sub_data = []
+        z = df[(df['Region'] == f"{region}")]
+        for discount in discount_values:
+            l = []
+            for sub_category in sub_category_values:
+                a = z[(z['Sub-Category'] == f'{sub_category}')]
+                a = a[(a['Discount'] == f'{discount}')]
+                l.append(len(a.index))
+                print(f"{region}:{sub_category}:{discount}:{len(a.index)}")
+            series_drilldown_sub_data.append(l)
+        charts_drilldown_sub_list.append(series_drilldown_sub_data)
+
+    print(charts_drilldown_sub_list)
+
+    return discount_values, region_values, series_data, category_values, charts_drilldown_list, sub_category_values, charts_drilldown_sub_list
 
 
 def _filter_df_by_date(df, date_column, from_month, from_year, to_month,
